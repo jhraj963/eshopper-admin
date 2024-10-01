2f769c91f4d1260fdd94208e8bc619db953bbb53
jhraj963 <jhraj963@gmail.com>
2024-10-01 13:56:09 +0600

    login api

--- src/Api/AllApi.js
+++ src/Api/AllApi.js
@@ -1,39 +1,52 @@
 import axios from 'axios';
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const login = async (data) => {
-    const LOGIN_ENDPOINT = `http://localhost/eshopper/login.php`;
-    try{
-        let response = await axios.post(LOGIN_ENDPOINT, data);
-        if(response.data.jwt){
-            localStorage.setItem("access_token", response.data.jwt);
-            localStorage.setItem("userdata", response.data.datas);
+    const LOGIN_ENDPOINT = `${process.env.REACT_APP_API_URL}/login`;
+    try {
+        let response = await axios({
+            method: 'post',
+            url: LOGIN_ENDPOINT,
+            data: data
+        });
+
+        if (response.data.data.token) {
+            localStorage.setItem("access_token", response.data.data.token);
+            localStorage.setItem("userdata", response.data.data.data);
             return true;
-        }else{
+        } else {
             return false;
         }
-    }catch(e){
-        return false;
+    } catch (e) {
+        // return false;
         console.log(e);
     }
 }
-const register = async (data)=> {
-    const SIGNUP_ENDPOINT = `http://localhost/eshopper/register.php`;
-    try{
-        let response= await axios({
+const register = async (data) => {
+    const SIGNUP_ENDPOINT = `${process.env.REACT_APP_API_URL}/register`;
+    try {
+        let response = await axios({
             method: 'post',
             responsiveTYpe: 'json',
             url: SIGNUP_ENDPOINT,
-            data: data
+            data: data,
+            withCredentials: true,
+            headers: {
+                Accept: "application/json",
+                "X-Request-With": "XMLHttpRequest",
+                'Access-Control-Allow-origin': '*',
+                "Content-Type": 'multipart/form-data'
+            }
         });
         console.log(response);
-    } 
-    catch(e){
+    }
+    catch (e) {
         console.log(e);
     }
 }
-const logout = ()=>{
+const logout = () => {
     localStorage.removeItem("access_token");
     localStorage.removeItem("userdata");
 }
-export  { login, register, logout }
+
+export { login, register, logout }
