596421f13ca2c92b8c5e8376ab2eb0e0439f90d7
jhraj963 <jhraj963@gmail.com>
2024-10-24 01:42:32 +0600

    All Order add

--- src/pages/Allorders/index.js
+++ src/pages/Allorders/index.js
@@ -28,13 +28,16 @@ function Allorders() {
           <div class="card">
             <div class="card-body">
               <h2>All Orders</h2>
-              <Link to={'/allorder/add'} className='btn btn-success float-end' >Add New Order</Link>
+              {/* <Link to={'/allorder/add'} className='btn btn-success float-end' >Add New Order</Link> */}
               <table className="table table-bordered mb-0 text-center table-info">
                 <thead>
                   <tr>
                     <th>Order ID</th>
                     <th>Customer Name</th>
-                    <th>Date</th>
+                    <th>Email</th>
+                    <th>Mobile No</th>
+                    <th>Address</th>
+                    <th>OrderDate</th>
                     <th>Total Amount</th>
                     <th>Status</th>
                     <th>Action</th>
@@ -43,14 +46,18 @@ function Allorders() {
                 <tbody>
                     {data && data.map((d, key) =>
                         <tr key={d.id}>
-                            <td>{d.order_id}</td>
+                            <td>{d.id}</td>
                             <td>{d.customer_name}</td>
+                            <td>{d.email}</td>
+                            <td>{d.mobile_no}</td>
+                            <td>{d.address}</td>
                             <td>{d.order_date}</td>
                             <td>{d.total_amount}</td>
                             <td>{d.status}</td>
                             <td>
                                 <Link to={`/allorder/edit/${d.id}`} className='btn btn-info' >Edit</Link>
-                                <button type='button' onClick={() => deleteData(d.id)} className='btn btn-danger'>Delete</button>
+                                <Link to={`/allorder/edit/${d.id}`} className='btn btn-success' >Invoice</Link>
+                                {/* <button type='button' onClick={() => deleteData(d.id)} className='btn btn-danger'>Delete</button> */}
                             </td>
                         </tr>
                     )}
